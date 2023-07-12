4909d3889b9cd85cfe49bf0bd7c39bc164ba9883
Vadym Dubniak <dubnyakv@gmail.com>
2023-07-13 00:37:22 +0300

    home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,21 @@
 export const Home = () => {
   return (
-    <div>
-      home page
+    <div className="home-page">
+      <h1 className="home-page__title">
+        Home page
+      </h1>
+
+      <p className="home-page__paragraph">
+        Welcome to Order & Products app!
+      </p>
+
+      <p className="home-page__paragraph">
+        You can start shoping going to &apos;
+        <span className="home-page__accented-text">
+          Products
+        </span>
+        &apos; page
+      </p>
     </div>
   );
 };
